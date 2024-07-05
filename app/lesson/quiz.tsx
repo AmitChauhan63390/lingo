2f9cfecd1c91d36a4d946a70d6bab66425b02e69
amit <amit63390@gmail.com>
2024-07-06 02:58:38 +0530

    lesson header

---
+++ app/lesson/quiz.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { challengeOptions, challenges } from "@/db/schema";
+import { useState } from "react";
+import { Header } from "./header";
+
+type Props = {
+    initialPercentage: number;
+    initialHearts: number;
+    initialLessonId: number;
+    initialLessonChallenges: (typeof challenges.$inferSelect & {
+        completed: boolean;
+        challengeOptions: typeof challengeOptions.$inferSelect[];
+    })[];
+    userSubscription: any;
+};
+
+export const Quiz = ({ initialPercentage,
+    initialHearts,
+    initialLessonId,
+    initialLessonChallenges,
+    userSubscription,
+}: Props) => {
+    const [hearts,setHearts] = useState(initialHearts);
+    const [percentage,setPercentage] = useState(initialPercentage);
+    return <>
+    <Header
+    percentage={percentage}
+    hearts={hearts}
+    hasActiveSubscription={!!userSubscription?.isActive}/>
+    </>
+
+
+}
